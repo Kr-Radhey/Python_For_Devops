1e44a33dda4c941edd53ca7e54bbd80d899c2176
Radhey <virendrakumar479@gmail.com>
2024-01-22 10:58:42 +0530

    Slicing list

--- list.py
+++ list.py
@@ -1,5 +1,15 @@
+#printing reverse using negative index traversing
+
 list1= [2,3,4,5,6]
 i=-1
 for li in list1 :
-    print(list1[i])
-    i = i-1
+        print(list1[i])
+        i = i-1
+
+
+
+#slicing
+    
+new_list = ["this", "is", "list", "for", "slicing"]
+print(new_list[1:3])
+print(new_list[3:])
