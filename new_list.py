4067d8c8024c660b4671c5073130a5a174f9304a
Radhey <virendrakumar479@gmail.com>
2024-01-23 10:40:41 +0530

    Deletion on list using del statement

---
+++ new_list.py
@@ -0,0 +1,18 @@
+#Deletion using del statement
+
+list1 = list(input("Please enter list items: "))
+#index = int(input("Please input index number which you want to remove : "))
+print("List items before deletion : ", list1)
+
+
+#del list1[index]
+#print("List after deletion : ", list1)
+
+#Deleting blank spaces
+i=0
+for blank in list1 :
+    if blank == ' ' :
+        del list1[i]                 #del statement
+    i += 1
+
+print("List after removing blank spaces : ", list1)
